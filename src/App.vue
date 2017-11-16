135651f3ff05eeb36b3fd908b569cd90478f561f
Sergey Raspopov <raspopov.sergey.na@gmail.com>
2017-11-17 01:56:54 +0300

    49

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <nav-header @pageWasChanged="currentPage = $event"></nav-header>
+        {{currentPage}}
+        <card-front></card-front>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  import CardFront from './components/card/CardFront.vue'
+
+  export default {
+    data: function () {
+      return {
+        currentPage: 'card'
+      }
+    },
+    components: {
+      navHeader: Header,
+      cardFront: CardFront
+    }
+  }
+</script>
+
+<style>
+  .body {
+    font-family: Verdana, Gevena, sans-serif;
+    color: #333;
+  }
+  a {
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 class="text-center">Creative Cards</h1>
+    <ul class="nav justify-content-center">
+      <li class="nav-item"><a class="nav-link" @click="pageSelected='cardFront'">Font</a></li>  <!--когда надали на ссылку, запишет 'cardFront' в pageSelected-->
+      <li class="nav-item"><a class="nav-link" @click="pageSelected='cardInsideLeft'">Inside Left</a></li>
+      <li class="nav-item"><a class="nav-link" @click="pageSelected='cardInsideRight'">Inside Right</a></li>
+      <li class="nav-item"><a class="nav-link" @click="pageSelected='cardBack'">Back</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        pageSelected: ''
+      }
+    },
+    watch: {
+      pageSelected: function () {  //вызовется при изменении pageSelected
+        this.$emit('pageWasChanged', this.pageSelected) //порождает событие pageWasChanged с аргументом this.pageSelected
+      }
+    }
+  }
+</script>
+
+<style>
+  h1 {
+    margin-top: 10px;
+  }
+</style>
